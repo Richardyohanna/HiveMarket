e47398fef0b92a577d2d637766045370442cc8be
Richard Yohanna <yohanrichard091@gmail.com>
2026-06-07 07:11:05 +0100

    remove dotenv

--- Backend/backend/src/main/java/com/hivemarket/BackendApplication.java
+++ Backend/backend/src/main/java/com/hivemarket/BackendApplication.java
@@ -10,18 +10,7 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 		
-		Dotenv dotenv = Dotenv.load();
 
-        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-
-        System.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME"));
-        System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
-        System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
-
-        System.setProperty("PAYSTACK_SECRET_KEY", dotenv.get("PAYSTACK_SECRET_KEY"));
-        
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
